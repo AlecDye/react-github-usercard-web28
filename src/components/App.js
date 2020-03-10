e49bf53adec69c8b9c9444002cd1785b30106ba6
Alec Dye <alecdye@gmail.com>
2020-03-10 12:06:53 -0700

    Building state and api requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 import React from "react";
+import axios from "axios";
+
+// Components
+import Header from "./Header";
+import UserCard from "./UserCard";
 
 class App extends React.Component {
+  // Initial State
   constructor() {
+    console.log("Constructor fired");
     super();
   }
+  // Api requests
+  componentDidMount() {
+    console.log("CDM fired");
+    // User's data
+    axios
+      .get("https://api.github.com/users/alecdye")
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+
+    // Followers from User's data
+    axios
+      .get("https://api.github.com/users/alecdye/followers")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+  // Rendering components and state
   render() {
-    return <div className="App"></div>;
+    console.log("Render fired");
+    return (
+      <div className="app-wrapper">
+        <Header />
+        <div>
+          <UserCard />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/UserCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserCard() {
+  return <div className="user-card"></div>;
+}
+
+export default UserCard;
